c97454267c903efc59fb553670abe52d4e63f43a
s-motto <brinamotto@gmail.com>
2024-07-30 23:27:41 +0200

    add switch case

--- src/org/eventi/gestore/java/Evento.java
+++ src/org/eventi/gestore/java/Evento.java
@@ -1,11 +1,12 @@
 package org.eventi.gestore.java;
 
-import java.util.Calendar;
+import java.time.LocalDate;
+
 
 public class Evento {
 	
 	private String titolo;
-	private Calendar data;
+	private LocalDate data;
 	private int postiTotali;
 	private int postiPrenotati;
 	
@@ -26,16 +27,16 @@ public class Evento {
 
 
 
-	public Calendar getData() {
+	public LocalDate getData() {
 		return data;
 	}
 
 
 
 
-	public void setData(Calendar data) {
-		this.data = data;
-		if(data.before(Calendar.getInstance())){
+	public void setData(LocalDate data) {
+		this.data=data;
+		if(data.isBefore(LocalDate.now())){
 			System.out.println("La data selezionata è già passata");
 		}
 	}
@@ -59,18 +60,22 @@ public class Evento {
 	
 	//costruttore
 
- Evento(String titolo, Calendar data, int postiTotali, int postiPrenotati){
+ Evento(String titolo, LocalDate data, int postiTotali, int postiPrenotati){
 	this.titolo=titolo;
 	this.data=data;
 	this.postiTotali=postiTotali;
 	this.postiPrenotati=0;
+	
+	if(postiTotali<1) {
+		System.out.println("L'evento deve avere almeno un posto disponibile");
+	}
  }
 
 	public void prenota() {
 		if(postiPrenotati>postiTotali) {
 			System.out.println("I posti che vuoi prenotare sono più dei posti disponibili");
 			
-		}else if(data.before(Calendar.getInstance())){
+		}else if(data.isBefore(LocalDate.now())){
 			System.out.println("Non puoi prenotare un evento già passato");
 		}else {
 			postiPrenotati++;
@@ -78,13 +83,11 @@ public class Evento {
 	}
 	
 	
-	public void disdici(int postiDisdetti) {
-		if(postiDisdetti>postiPrenotati) {
-			System.out.println("Non puoi disdire più posti di quelli che hai prenotato!");
-		}else {
+	public void disdici() {
+		
 			postiPrenotati--;
 		}
-	}
+	
 	
 	public String toString() {
 		return data+" - "+titolo;
